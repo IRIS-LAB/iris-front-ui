53483120fec6e2125f22fe85c54a0e9931b45ece
Alan Duchene <alan.duchene@systeme-u.fr>
2019-01-18 15:30:13 +0100

    feat(avatar): beginning

---
+++ src/components/user/IrisAvatar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="iris-user-style">
+    <avatar username="Jane Doe"></avatar>
+  </div>
+</template>
+<script>
+import IrisHelloWorld from '../helloworld/IrisHelloWorld'
+/**
+ * ### Description
+ *
+ * This a faboulous Helloworld component.
+ * @author Alan Duchêne
+ * @version 1.0.0
+ *
+ * ### Styling
+ *
+ *
+ * ### Properties and Methods
+ */
+export default {
+  name: 'iris-avatar',
+  components: { IrisHelloWorld },
+  props: {},
+  methods: {}
+}
+</script>
+<style scoped>
+</style>
+
+<docs>
+### Examples
+
+#### Simple Example
+
+```vue
+<iris-avatar username="Alan Duchêne"></iris-avatar>
+```
+</docs>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import './assets/styles/main.css'
 import Vue from 'vue'
 import Components from './components.js'
-import { Dialog, Icon } from 'element-ui'
+import { Dialog, Icon, Dropdown } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Icon)
 Vue.use(Dialog)
+Vue.use(Dropdown)
 
 Object.keys(Components).forEach(key => {
   Vue.component(Components[key].name, Components[key])
